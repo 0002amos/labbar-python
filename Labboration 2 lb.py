a03193ac4c5dd55183bcd9f35187612f27d12a7d
Ambjorn <ambjorn.osinga@gmail.com>
2018-12-14 15:15:39 +0100

    Working without import

---
+++ Labboration 2 lb.py
@@ -0,0 +1,65 @@
+"""game mastermind"""
+import random
+import master_mind_GUI
+from leaderboard import *
+
+def create_random():
+    """Creates list of 4 letters"""
+    target = []
+    target = (random.sample(range(0, 6), 4))
+    target = [1,2,3,4]
+    return target
+
+def check_right(target, guesses):
+    """Checks right place and right color"""
+    right_color = 0
+    right_place = 0
+
+    check = []
+    for position in range(4):
+        if guesses[position] == target[position]:
+            right_place += 1
+            check.append(guesses[position])
+
+    for position in range(4):
+        if ((guesses[position] not in check) and (guesses[position] in target)):
+            right_color += 1
+            check.append(guesses[position])
+
+    return (right_place, right_color)
+
+def master_mind(target):
+    """The game"""
+    game_window = master_mind_GUI.create_GUI()
+
+    rounds = 0
+    playing = True
+
+    while playing:
+
+        guesses = master_mind_GUI.make_guess(rounds, game_window)
+
+        right_wrong = check_right(target, guesses)
+
+        master_mind_GUI.peg_feedback(rounds, right_wrong[0], right_wrong[1], game_window)
+        rounds += 1
+
+        if guesses == target:
+            master_mind_GUI.gameover_screen(rounds, "Winner")
+            
+            score = getscore(rounds)
+            
+            new_list = add_and_sort(import_list(), get_new_score_name(), score)
+
+            return(new_list)
+
+        elif rounds == 7:
+            master_mind_GUI.gameover_screen(rounds, "Looser")
+            playing = False
+
+lb_win = create_window()
+show_lb(import_list(), lb_win)
+new_board = master_mind(create_random())
+clear(lb_win)
+show_lb(new_board, lb_win)
+lb_win.getMouse()
--- leaderboard.py
+++ leaderboard.py
@@ -1,43 +1,42 @@
-from operator import itemgetter
-from master_mind_GUI import *
+from operator import itemgetter 
 import graphics as g
 
-LBwindow = g.GraphWin("score",200 ,400)
+def create_window():
+    LBwindow = g.GraphWin("score",200 ,400)
+    return LBwindow
 
 def clear(win):
     for item in win.items[:]:
         item.undraw()
     win.update()
 
-def show_lb(scores):
+def show_lb(scores, LBwindow):
     
-    clear(LBwindow)
-
-    over_text = Text(Point(100,50),"Score   name")
+    over_text = g.Text(g.Point(100,50),"Score   name")
     score1 = str(scores[0]["score"])
     name1 = scores[0]["name"]
-    player1 = score1 +"     " + name1
-    player1p = Text(Point(100, 100), player1)
+    player1 = score1 + "     " + name1
+    player1p = g.Text(g.Point(100, 100), player1)
 
     score2 = str(scores[1]["score"])
     name2 = scores[1]["name"]
-    player2 = score2 +"     " + name2
-    player2p = Text(Point(100, 150), player2)
+    player2 = score2 + "     " + name2
+    player2p = g.Text(g.Point(100, 150), player2)
 
     score3 = str(scores[2]["score"])
     name3 = scores[2]["name"]
-    player3 = score3 +"     " + name3
-    player3p = Text(Point(100, 200), player3)
+    player3 = score3 + "     " + name3
+    player3p = g.Text(g.Point(100, 200), player3)
     
     score4 = str(scores[3]["score"])
     name4 = scores[3]["name"]
-    player4 = score4 +"     " + name4
-    player4p = Text(Point(100, 250), player4)
+    player4 = score4 + "     " + name4
+    player4p = g.Text(g.Point(100, 250), player4)
 
     score5 = str(scores[4]["score"])
     name5 = scores[4]["name"]
-    player5 = score5 +"     " + name5
-    player5p = Text(Point(100, 300), player5)
+    player5 = score5 + "     " + name5
+    player5p = g.Text(g.Point(100, 300), player5)
 
     over_text.setSize(10)
     player1p.setSize(10)
@@ -53,21 +52,38 @@ def show_lb(scores):
     player4p.draw(LBwindow)
     player5p.draw(LBwindow)
 
-
 def get_new_score_name():
     name_window = g.GraphWin("Name",200, 200)
-
-    get_name = Text(g.Point(100, 80), "Skriv ditt namn här")
-    enter = Entry(g.Point(100,100), 5)
-
-    enter.draw(name_window)
-    
-    name_window.getMouse()
+    """
+    get_name = g.Text(g.Point(100, 80), "Skriv ditt namn här")
+    enter = g.Entry(g.Point(100,100), 5)
     
     get_name.draw(name_window)
-    drawtxt_ = enter.getText()
+    enter.draw(name_window)
     
+    drawtxt_ = enter.getText()
+    name_window.getMouse
+
+    name_window.close()
     return drawtxt_
+    """
+    get_name = g.Text(g.Point(100,80), "Name")
+    click_conf = g.Text(g.Point(100,120), "Click to continue")
+
+    name_enter = g.Entry(g.Point(100, 100), 10)
+
+    get_name.draw(name_window)
+    click_conf.draw(name_window)
+    name_enter.draw(name_window)
+
+    name_window.getMouse()
+
+    name = str(name_enter.getText())
+
+    name_window.close()
+
+    return (name)
+
 
 def add_and_sort(scores, new_name, new_score):
     newlist = sorted(scores, key=itemgetter("score"), reverse=True)
@@ -92,10 +108,30 @@ def import_list():
     hs = [player1, player2, player3, player4, player5]
 
     return hs
+
+def save_new(list_save):
+    return
+
+def getscore(rounds):
+    if rounds == 1:
+        return 64
+    elif rounds == 2:
+        return 32
+    elif rounds == 3:
+        return 16
+    elif rounds == 4:
+        return 8
+    elif rounds == 5:
+        return 4        
+    elif rounds == 6:
+        return 2
+    elif rounds == 7:
+        return 1
+
 #player6 = {"score":5, "name":"amos"}
 
-show_lb(import_list())
+#show_lb(import_list())
 
-show_lb(add_and_sort(import_list(), get_new_score_name(), 50))
+#show_lb(add_and_sort(import_list(), get_new_score_name(), 50))
 
-LBwindow.getMouse()
+#LBwindow.getMouse()
