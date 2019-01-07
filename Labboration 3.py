dafe64a1b93b2a472b8e5d1dab99c04e048a9ff9
Ambjorn <ambjorn.osinga@gmail.com>
2019-01-07 12:50:51 +0100

    Better names

--- Labboration 3.py
+++ Labboration 3.py
@@ -30,31 +30,33 @@ def remove_triangle(side_lenght, middle, win):
     shape.setFill("White")
     shape.draw(win)
 
-    #To shorten argument for functions
-    side_lenght = side_lenght/4                     #New side lenght to send into function
-    x_plus_side = middle.getX() + side_lenght       #Set to middle + new lenght
-    x_minus_middle = middle.getX() - side_lenght    #Set to middle - new lenght
-    x_minus2 = middle.getX() - side_lenght/2        #Set to lenght/2 to the left
-    x_plus2 = middle.getX() + side_lenght/2         #Set to lenght/2 to the right
-    yp_1_8 = middle.getY() + vert_lenght/8          #Set to lenght/8  down
-    yp_3_8 = middle.getY() + vert_lenght*3/8        #Set to lenght*(3/8)  down
-
     #Help for next part
     #           1
     #         2   3
     #       4       5
     #     6   7   8   9
-    #Here recursive cals are made for triangles around the one triangle that was just drawn
+ 
+
+    #To shorten argument for functions
+    side_lenght = side_lenght/4                         #New side lenght to send into function
+    X_point_5 = middle.getX() + side_lenght             #Set x för point 5
+    X_point_4 = middle.getX() - side_lenght             #Set x för point 4
+    x_point_2_7 = middle.getX() - side_lenght/2         #Set x för point 2 and 7
+    x_point_3_8 = middle.getX() + side_lenght/2         #Set x for point 3 and 8
+    yp_1_8 = middle.getY() + vert_lenght/8              #Set y for point 4 and 5
+    Y_point_6_7_8_9 = middle.getY() + vert_lenght*3/8   #Set y for point 6, 7, 8 and 9
+
+   #Here recursive cals are made for triangles around the one triangle that was just drawn
 
     remove_triangle(side_lenght, g.Point(middle.getX(), middle.getY() - vert_lenght/2*(3/4)), win)
-    remove_triangle(side_lenght, g.Point(x_minus2, middle.getY() - vert_lenght/2*(1/4)), win)
-    remove_triangle(side_lenght, g.Point(x_plus2, middle.getY() - vert_lenght/2*(1/4)), win)
-    remove_triangle(side_lenght, g.Point(x_minus_middle, yp_1_8), win)
-    remove_triangle(side_lenght, g.Point(x_plus_side, yp_1_8), win)
-    remove_triangle(side_lenght, g.Point(middle.getX() - side_lenght*1.5, yp_3_8), win)
-    remove_triangle(side_lenght, g.Point(x_minus2, yp_3_8), win)
-    remove_triangle(side_lenght, g.Point(x_plus2, yp_3_8), win)
-    remove_triangle(side_lenght, g.Point(middle.getX() + side_lenght*1.5, yp_3_8), win)
+    remove_triangle(side_lenght, g.Point(x_point_2_7, middle.getY() - vert_lenght/2*(1/4)), win)
+    remove_triangle(side_lenght, g.Point(x_point_3_8, middle.getY() - vert_lenght/2*(1/4)), win)
+    remove_triangle(side_lenght, g.Point(X_point_4, yp_1_8), win)
+    remove_triangle(side_lenght, g.Point(X_point_5, yp_1_8), win)
+    remove_triangle(side_lenght, g.Point(middle.getX() - side_lenght*1.5, Y_point_6_7_8_9), win)
+    remove_triangle(side_lenght, g.Point(x_point_2_7, Y_point_6_7_8_9), win)
+    remove_triangle(side_lenght, g.Point(x_point_3_8, Y_point_6_7_8_9), win)
+    remove_triangle(side_lenght, g.Point(middle.getX() + side_lenght*1.5, Y_point_6_7_8_9), win)
 
 def sierpinski(win, size, start, color):
     """Calls both start_triangle funktion and remove_triangle function at once"""
